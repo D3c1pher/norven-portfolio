03419332c9e1d947af60712c0430ef79d317d390
norven.caracas <norven.caracas@gmail.com>
2024-03-22 16:44:08 +0800

    Updated navbar, cursor and hero

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,45 +1,81 @@
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 import { useTheme } from "../ThemeContext";
 
 export default function Cursor() {
   const { theme } = useTheme();
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = useState(false);
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
-      setPosition({ x: e.clientX, y: e.clientY });
-    };
+    const handleMouseEnter = () => setIsHovering(true);
+    const handleMouseLeave = () => setIsHovering(false);
 
-    // Adjust throttle time as needed
-    const throttledMouseMove = throttle(handleMouseMove, 16);
-
-    window.addEventListener("mousemove", throttledMouseMove);
+    document.querySelectorAll('button, a, [data-hoverable="true"]').forEach((element) => {
+      element.addEventListener("mouseenter", handleMouseEnter);
+      element.addEventListener("mouseleave", handleMouseLeave);
+    });
 
     return () => {
-      window.removeEventListener("mousemove", throttledMouseMove);
+      document.querySelectorAll('button, a, [data-hoverable="true"]').forEach((element) => {
+        element.removeEventListener("mouseenter", handleMouseEnter);
+        element.removeEventListener("mouseleave", handleMouseLeave);
+      });
     };
   }, []);
 
-  return (
-    <motion.div
-      className={`cursor bg-transparent w-12 h-12 rounded-full border-2 fixed z-50 hidden lg:block ${theme === "night" ? "border-white" : "border-black"}`}
-      style={{ left: position.x, top: position.y }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    ></motion.div>
-  );
-}
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const cursorDot = document.querySelector("[data-cursor-dot]");
+      const cursorOutline = document.querySelector("[data-cursor-outline]");
 
-// Throttle function to limit the frequency of function calls
-function throttle(func, delay) {
-  let lastCall = 0;
-  return function (...args) {
-    const now = new Date().getTime();
-    if (now - lastCall >= delay) {
-      func(...args);
-      lastCall = now;
-    }
-  };
+      if (cursorDot && cursorOutline) {
+        const posX = e.clientX;
+        const posY = e.clientY;
+
+        cursorDot.style.left = `${posX}px`;
+        cursorDot.style.top = `${posY}px`;
+
+        cursorOutline.animate({
+          left: `${posX}px`,
+          top: `${posY}px`,
+        }, { duration: 500, fill: "forwards" });
+
+        if (isHovering) {
+          cursorOutline.animate({
+            height: "100px",
+            width: "100px", 
+          }, { duration: 500, fill: "forwards" });
+        } else {
+          cursorOutline.animate({
+            height: "50px",
+            width: "50px", 
+          }, { duration: 500, fill: "forwards" });
+        }
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [isHovering]);
+
+  return (
+    <>
+      <div
+        id="cursor-dot"
+        className={`fixed top-0 left-0 w-[10px] h-[10px] ${
+          theme === "night" ? "bg-white" : "bg-black"
+        } transform -translate-x-1/2 -translate-y-1/2 rounded-full z-50 pointer-events-none`}
+        data-cursor-dot
+      ></div>
+      <div
+        id="cursor-outline"
+        className={`fixed top-0 left-0 border-2 ${
+          theme === "night" ? "border-white" : "border-black"
+        } transform -translate-x-1/2 -translate-y-1/2 rounded-full z-50 pointer-events-none`}
+        data-cursor-outline
+      ></div>
+    </>
+  );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,12 +35,17 @@ const sliderVariants = {
 
 export default function Hero() {
   return (
-    <div className="hero min-h-screen bg-gradient-to-b from-base-100 to-base-300 relative">
-      <div className="hero-content flex flex-col text-center lg:text-start">
+    <div className="hero min-h-screen relative">
+      
+      {/* Texture Overlay */}
+      <div className="texture-v2 min-h-screen absolute top-0 left-0 right-0 z-10 mix-blend-multiply pointer-events-none" />
+
+      {/* Hero Content */}
+      <div className="hero-content flex flex-col text-center lg:text-start py-16">
 
         {/* MOBILE IMAGE */}
         <div className="lg:hidden flex justify-center w-full lg:w-1/2">
-          <img src={Profile2} alt="Norven Profile Image" className="w-full h-full max-w-[360px] max-h-[360px] object-contain" />
+          <img src={Profile2} alt="Norven Profile Image" className="w-full h-full max-w-[360px] max-h-[360px] object-contain pointer-events-none" />
         </div>
 
         {/* CONTENT */}
@@ -52,13 +57,13 @@ export default function Hero() {
         >
           <motion.h1 variants={textVariants} className="text-2xl sm:text-3xl lg:text-4xl tracking-widest font-semibold text-primary">NORVEN <span className="hidden lg:inline">EPHRAIM</span> CARACAS</motion.h1>
           <motion.h2 variants={textVariants} className="text-3xl sm:text-5xl lg:text-7xl font-bold pt-2 lg:pt-5">Web Developer & <span className="block lg:pt-2">Graphic Artist</span></motion.h2>
-          <motion.div variants={textVariants} className="flex-col pt-5 justify-center lg:justify-start">
-            <motion.button variants={textVariants} className="btn btn-lg btn-outline btn-primary my-2 mx-2">ABOUT ME</motion.button>
-            <motion.button variants={textVariants} className="btn btn-lg btn-outline btn-secondary my-2 mx-2">CONTACT ME</motion.button>
+          <motion.div variants={textVariants} className="flex-col py-5 px-10 sm:px-0 justify-center lg:justify-start">
+            <motion.button variants={textVariants} className="btn btn-lg btn-primary btn-outline w-full sm:w-auto text-white font-bold my-2 mx-2">MY PROJECTS</motion.button>
+            <motion.button variants={textVariants} className="btn btn-lg btn-secondary btn-outline w-full sm:w-auto text-white font-bold my-2 mx-2">CONTACT ME</motion.button>
           </motion.div>
         </motion.div>
         <motion.div
-          className="absolute text-6xl md:text-9xl font-bold bottom-0 right-0 whitespace-nowrap opacity-10"
+          className="absolute text-6xl md:text-9xl font-bold bottom-0 right-0 whitespace-nowrap opacity-10 pointer-events-none"
           variants={sliderVariants}
           initial="initial"
           animate="animate"
@@ -67,13 +72,14 @@ export default function Hero() {
         </motion.div>
 
         {/* DESKTOP IMAGE */}
-        <div className="hidden lg:block h-full lg:absolute lg:pt-20 lg:pr-20 right-0 bottom-0 z-20">
-          <img src={Profile} alt="Norven Profile Image" className="w-full h-full object-cover" />
+        <div className="hidden lg:block h-full lg:absolute lg:pt-20 xl:pr-40 right-0 bottom-0 z-20 pointer-events-none">
+          <img src={Profile} alt="Norven Profile Image" className="w-full h-full object-cover pointer-events-none" />
         </div>
       </div>
 
       {/* CURVE DESIGN */}
-      <svg className="block self-end text-secondary" viewBox="0 0 1440 320"><path fill="currentColor" d="M0,256L120,266.7C240,277,480,299,720,256C960,213,1200,107,1320,53.3L1440,0L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+      <svg className="block self-end text-secondary pointer-events-none" viewBox="0 0 1440 320"><path fill="currentColor" d="M0,256L120,266.7C240,277,480,299,720,256C960,213,1200,107,1320,53.3L1440,0L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+
     </div>
   );
 };
